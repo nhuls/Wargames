9bc113c8b6665a76571e25af65f99b0299d9334a
nathuls <nathuls@uat.edu>
2018-02-05 18:29:06 -0700

    Results Display done

--- Wargames/Wargames.cpp
+++ Wargames/Wargames.cpp
@@ -12,48 +12,6 @@
 
 using namespace std;
 
-
-int main()
-{
-	srand(static_cast<unsigned int>(time(0)));		//seed random number generator
-
-	int targetLocation = rand() % 64 + 1;		//random number between 1 and 64; 
-	int LinearTries, BinaryTries, RandomTries, HumanTries = 0;
-
-	cout << "\tBeginning Wargames Simulation.\n\n";
-
-	HumanTries = Human(targetLocation);
-	LinearTries = LinearAI(targetLocation);
-	BinaryTries = BinaryAI(targetLocation);
-	RandomTries = RandomAI(targetLocation);
-	
-	/*
-	do
-	{
-		cout << "Enter a guess: ";
-		cin >> guess;
-		++tries;
-
-		if (guess > targetLocation)
-		{
-			cout << "Too high!\n\n";
-		}
-		else if (guess < targetLocation)
-		{
-			cout << "Too low!\n\n";
-		}
-		else
-		{
-			cout << "\nThat's it!  You got it in " << tries << " guesses!\n";
-		}
-	} while (guess != targetLocation);
-	*/
-
-	cout << "\tEnding Simulation.\n\n";
-	system("pause");
-	return 0;
-}
-
 int LinearAI(int _target)
 {
 	int tries = 0;
@@ -63,13 +21,19 @@ int LinearAI(int _target)
 	do
 	{
 		tries++;
+		cout << "Scanning..." << endl;
 		//get prediction
 		prediction = tries; 
 
 		//test prediction
+		if (prediction != target)
+		{
+			cout << "Not Found." << endl;
+		}
 
 	} while (prediction != target);
 
+	cout << "Target Found." << endl;
 	return tries;
 }
 
@@ -84,35 +48,56 @@ int BinaryAI(int _target)
 	do
 	{
 		tries++;
+		cout << "Scanning..." << endl;
 		//get prediction
-		int prediction = ((highParameter - lowParameter) / 2) + lowParameter; 
+		prediction = ((highParameter - lowParameter) / 2) + lowParameter; 
 
 		//test prediction
-
+		if (prediction < target)	//if prediction too low
+		{
+			//adjust lowParameter
+			lowParameter = prediction + 1;
+		}
+		else if (prediction > target)	// if prediction too high
+		{
+			//adjust highParameter
+			highParameter = prediction - 1; 
+		}
 	} while (prediction != target);
 
+	cout << "Target Found." << endl;
 	return tries;
 }
+
 int RandomAI(int _target)
 {
 	int tries = 0;
 	int target = _target;
 	int prediction;
 
+	cout << "Please Wait..." << endl;
 	do
 	{
 		tries++;
+		//cout << "Scanning..." << endl;
 		//get prediction
 		srand(static_cast<unsigned int>(time(0)));		//seed random number generator
 		prediction = rand() % 64 + 1;		//random number between 1 and 64; 
 
+		/*
 		//test prediction
-
+		if (prediction != target)
+		{
+			cout << "Not Found." << endl;
+		}
+		*/
 
 	} while (prediction != target);
 
+	cout << "Target Found." << endl;
 	return tries;
 }
+
 int Human(int _target)
 {
 	int tries = 0;
@@ -127,8 +112,70 @@ int Human(int _target)
 		cin >> prediction; 
 
 		//test prediction
+		if (prediction < target)
+		{
+			cout << "Prediction too low." << endl;
+		}
+		else if (prediction > target)
+		{
+			cout << "Prediction too high." << endl;
+		}
 
 	} while (prediction != target);
 
+	cout << "Target Found." << endl;
 	return tries;
+}
+
+int main()
+{
+	srand(static_cast<unsigned int>(time(0)));		//seed random number generator
+
+	int targetLocation = rand() % 64 + 1;		//random number between 1 and 64; 
+	int linearTries, binaryTries, randomTries, humanTries = 0;
+
+	cout << "\tBeginning Wargames Simulation.\n\n";
+
+	//run each test in sequence
+	cout << "Running Human Test." << endl;
+	humanTries = Human(targetLocation);
+	cout << "Running Linear AI Test." << endl;
+	linearTries = LinearAI(targetLocation);
+	cout << "Running Binary AI Test." << endl;
+	binaryTries = BinaryAI(targetLocation);
+	cout << "Running Random AI Test." << endl;
+	randomTries = RandomAI(targetLocation);
+	cout << "All Tests Complete." << endl << endl;
+
+	//display results
+	cout << "\tResults: \n";
+	cout << "The Human found the target in " << humanTries << " guesses." << endl;
+	cout << "The Linear AI found the target in " << linearTries << " guesses." << endl;
+	cout << "The Binary AI found the target in " << binaryTries << " guesses." << endl;
+	cout << "The Random AI found the target in " << randomTries << " guesses." << endl;
+
+	/*
+	//determine winner
+	if (humanTries < linearTries && humanTries < binaryTries && humanTries < randomTries)	//if humanTries is lowest
+	{
+		cout << "The Human Wins! \n\n"; 
+	}
+	else if (linearTries < humanTries && linearTries < binaryTries && linearTries < randomTries)	//if linearTries is lowest
+	{
+		cout << "The Linear AI Wins! \n\n";
+	}
+	else if (binaryTries < humanTries && binaryTries < linearTries && binaryTries < randomTries)	//if binaryTries is lowest
+	{
+		cout << "The Binary AI Wins! \n\n";
+	}
+	else if (randomTries < humanTries && randomTries < linearTries && randomTries < binaryTries)	//if randomTries is lowest
+	{
+		cout << "The Random AI Wins! \n\n";
+	}
+	*/
+
+	//end program
+	cout << "\tEnding Simulation.\n\n";
+	system("pause");
+	return 0;
 }
